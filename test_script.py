6999500e71a82a045dc8fae946df35d7839edd6c
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2013-09-30 02:09:48 +0200

    one more commit

--- test_script.py
+++ test_script.py
@@ -16,7 +16,7 @@ def g():
     """
     TODO: write something already!
     """
-    pass  # TODO: *WANNA* MARKDOWN HERE
+    pass  # TODO: *WANNA* MARKDOWN HER
 
 def y():
     return 42 # TODO: NOTHING!!
